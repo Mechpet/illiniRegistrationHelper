644f413b26854ec934620dffbb0b2bcb178a2ef3
Mechpet <blazepet2@gmail.com>
2021-03-03 18:56:05 -0600

    Version 0.1.2 - Added GPA color setting

--- popup.js
+++ popup.js
@@ -3,11 +3,14 @@
 /* set to storage the new user setting value */
 function setSaved() {
     let keyName, keyObj;
-    let currSetting = $(this);
+    let currSetting = $(this);  // $(this) is a div.setting element
     keyName = currSetting.attr("id");
     keyObj = {};
     if (currSetting.hasClass("radios")) {
         keyObj[keyName] = currSetting.find("input:checked").val();
+    }
+    else if (currSetting.hasClass("color")) {
+        keyObj[keyName] = currSetting.find("#lower").val() + "|" + currSetting.find("#upper").val();
     }
     else {
         keyObj[keyName] = (currSetting.find("input").is(":checked")) ? "checked" : "unchecked";
@@ -44,17 +47,76 @@ function updateRadio(keyObj, setting) {
     }
 }
 
+function updateRange(keyObj, setting) {
+    if (Object.values(keyObj)[0]) {
+        console.log("Got setting: Obj is ", keyObj, "Value is", Object.values(keyObj)[0]);
+        let values = Object.values(keyObj)[0].split("|");
+        setting.find("#lower").val(values[0]);
+        setting.find("#upper").val(values[1]);
+        moveRange();
+    }
+}
+
 /* get the saved user settings, and update each element that is related to settings */
 function getSaved() {
     let allInputs = $("div.setting");
     let setting;
+    let callback;
     allInputs.each(function() {
         setting = $(this);
-        getKeyValue(setting.attr("id"), (setting.hasClass("radios")) ? updateRadio : updateSwitch, setting);
+        if (setting.hasClass("radios")) {
+            callback = updateRadio;
+        }
+        else if (setting.hasClass("color")) {
+            callback = updateRange;
+        }
+        else {
+            callback = updateSwitch;
+        }
+        getKeyValue(setting.attr("id"), callback, setting);
     });
 }
 
+function moveRange() {
+    // Limit input to [0.0, 4.0]
+    if ($(this).val() > 4.0)
+        $(this).val(4.0);
+    else if ($(this).val() < 0.0)
+        $(this).val(0.0);
+    // Move slider to the correct values
+    $("#range").slider("values", 0, $("#lower").val());
+    $("#range").slider("values", 1, $("#upper").val());
+    backgroundGradient();
+}
+
+function backgroundGradient() {
+    let firstThreshold = $("#lower").val() / 4.0 * 100;
+    var newStyle = document.createElement("style");
+    newStyle.innerHTML = 
+    `.ui-slider {
+        background-image: linear-gradient(to right, hsl(0,100%,50%), hsl(0,100%,50%) ` + String(firstThreshold) + `%,
+        hsl(120,100%,50%) ` + String(firstThreshold) + `%);
+    }`
+    document.body.appendChild(newStyle);
+}
+
 $(function() {
+    $("#range").slider({
+        range: true,
+        min: 0,
+        max: 4.0,
+        step: 0.01,
+        values: [2.0, 4.0],
+        slide: function(event, ui) { // On slide, update the input values
+          $("#lower").val(ui.values[0]);
+          $("#upper").val(ui.values[1]);
+        },
+        stop: function(event, ui) { // When the slide has stopped, save the new values
+            $(".color").each(setSaved);
+            backgroundGradient();
+        }
+    });
+    $("input[type=\"number\"]").on("change", moveRange);
     getSaved();
     $("div.setting").on("change", setSaved);
 })
